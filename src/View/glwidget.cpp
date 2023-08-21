9c33f58c76d6b1ec5058c8ef6f98690d2a915f8d
Janiece Eleonor <janiecee@un-j4.kzn.21-school.ru>
2023-08-21 20:15:06 +0300

    LIGHT AND TEXTURES INIT

--- src/View/glwidget.cpp
+++ src/View/glwidget.cpp
@@ -7,6 +7,37 @@ GLWidget::GLWidget(QWidget *parent) : QOpenGLWidget(parent) {
   connect(paintTimer, &QTimer::timeout, this, QOverload<>::of(&GLWidget::repaint));
 }
 
+void GLWidget::initTextures(uint index, QImage &textureImg) {
+    textureImg.convertTo(QImage::Format_RGBA8888);
+    glBindTexture(GL_TEXTURE_2D, texture[index]);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, 1, GLsizei(textureImg.width()), GLsizei(textureImg.height()), 0, GL_RGBA, GL_UNSIGNED_BYTE, textureImg.bits());
+}
+
+void GLWidget::loadGLTextures() {
+    glGenTextures(3, texture);
+
+    QImage textureImg;
+    textureImg.load("/Users/janiecee/Documents/CPP4_3DViewer_v2.0-0/src/Obj/MetalCladdingFrame002/BUMP_1K.jpg");
+    initTextures(0, textureImg);
+}
+
+void GLWidget::initLight() {
+    GLfloat lightAmbient[] = { 0, 0, 0, 0.0 };
+    GLfloat lightDiffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat lightPosition[] = { 0.0, 0.0, 2.0, 1.0 };
+
+    // setup parameters of light
+    glLightfv(GL_LIGHT0, GL_AMBIENT, lightAmbient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, lightDiffuse);
+    glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
+
+    // enable of light
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+}
+
 void GLWidget::initializeGL() {
     glEnable(GL_MULTISAMPLE);
     loadGLTextures();
@@ -17,10 +48,10 @@ void GLWidget::initializeGL() {
     glDepthFunc(GL_LESS);
     glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
     initLight();
-//    model[0] = ObjLoader::instance().load("/Users/janiecee/Documents/CPP4_3DViewer_v2.0-0/src/Obj/Cow.obj");
-//    model[1] = ObjLoader::instance().load("/Users/janiecee/Documents/CPP4_3DViewer_v2.0-0/src/Obj/cube.obj");
+    model[0] = ObjLoader::instance().load("/Users/janiecee/Documents/CPP4_3DViewer_v2.0-0/src/Obj/Cow.obj");
+    model[1] = ObjLoader::instance().load("/Users/janiecee/Documents/CPP4_3DViewer_v2.0-0/src/Obj/cube.obj");
     model[0] = drawCube();
-//    torus = ObjLoader::instance().load("/Users/janiecee/Documents/CPP4_3DViewer_v2.0-0/src/Obj/Teddy.obj");
+    torus = ObjLoader::instance().load("/Users/janiecee/Documents/CPP4_3DViewer_v2.0-0/src/Obj/Teddy.obj");
 }
 
 void GLWidget::resizeGL(int nWidth, int nHeight) {
@@ -36,4 +67,19 @@ void GLWidget::resizeGL(int nWidth, int nHeight) {
     // init matrix model view
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
+}
+
+void GLWidget::paintGL() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    glBindTexture(GL_TEXTURE_2D, texture[2]);
+    glTranslatef(0,0,-2.5);
+    glDeleteLists(torus, 1);
+    torus = ObjLoader::Instance().draw(angle/100);
+    glCallList(torus);
+    glTranslatef(0,0,0.1f);
+    glRotatef(angle, 0.0f, 1.0f, 0.0f);
+    glBindTexture(GL_TEXTURE_2D, texture[textureCount]);
+    glCallList(model[modelCount]);
+    angle += 0.3f;
 }
